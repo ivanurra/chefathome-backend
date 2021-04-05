ed62100b89d405d2a4e93c36d1d5850e7253ac19
Ivan Urra <ivan.urra.wd@gmail.com>
2021-04-05 08:27:53 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8d97f214efc131a2d3c649146de6f100'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '724c5412-1fbd-4183-a80f-7c03bce3ae47'
+};
